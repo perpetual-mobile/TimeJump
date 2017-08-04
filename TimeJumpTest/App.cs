d29fd5a5c509f6c4777dc947e23ebb1ef41b4d91
Falko Schindler <mail@falkoschindler.de>
2017-08-04 12:47:56 +0200

    Initial commit

---
+++ TimeJumpTest/App.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TimeJumpTest
+{
+    public class App : Application
+    {
+        TimeSpan period = TimeSpan.FromMilliseconds(1);
+
+        public App()
+        {
+            var minLabel = new Label();
+            var maxLabel = new Label();
+
+            var minTimeSpan = TimeSpan.MaxValue;
+            var maxTimeSpan = TimeSpan.MinValue;
+
+            MainPage = new ContentPage {
+                Padding = 20,
+                Content = new StackLayout {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        minLabel,
+                        maxLabel,
+                        new Button {
+                            Text = "Reset",
+                            Command = new Command(obj => {
+                                minTimeSpan = TimeSpan.MaxValue;
+                                maxTimeSpan = TimeSpan.MinValue;
+                            }),
+                        },
+                    },
+                },
+            };
+
+            MainPage.Appearing += async delegate {
+
+                while (true) {
+
+                    var now = DateTime.Now;
+                    await Task.Run(() => Thread.Sleep(period));
+                    var then = DateTime.Now;
+
+                    var elapsed = then - now;
+                    if (elapsed < minTimeSpan) {
+                        minTimeSpan = elapsed;
+                        minLabel.Text = string.Format("Min: {0:0.000} ms", minTimeSpan.TotalMilliseconds);
+                    }
+                    if (elapsed > maxTimeSpan) {
+                        maxTimeSpan = elapsed;
+                        maxLabel.Text = string.Format("Max: {0:0.000} ms", maxTimeSpan.TotalMilliseconds);
+                    }
+
+                    if (elapsed < period)
+                        Console.WriteLine("Short timespan: {0:0.000} ms elapsed", elapsed.TotalMilliseconds);
+                    if (elapsed > TimeSpan.FromMilliseconds(10 * period.TotalMilliseconds))
+                        Console.WriteLine("Long timespan: {0:0.000} ms elapsed", elapsed.TotalMilliseconds);
+                }
+
+            };
+        }
+    }
+}
